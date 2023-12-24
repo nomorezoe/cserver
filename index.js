bfffd2171cbac9129b5827ae8625626c21e281f9
nomorezoe <zoewangwork@gmail.com>
2023-12-24 13:40:29 +0800

    e

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get('/server', (req, res) => {
     });
 })
 
-async function processUpscale(file, session, res){
+async function processUpscale(file, imageFileName, session, res){
     fs.readFile(file, (err, data)=>{
         const tags =  ExifReader.load(data);
         var model = "dynavisionXL.safetensors";
@@ -106,7 +106,7 @@ app.post('/upscale', (req, res) => {
     console.log("imageFileName"+imageFileName);
 
     var filePath = __dirname + '/../fileserver/output/'+imageFileName;
-    processUpscale(filePath, session, res);
+    processUpscale(filePath, imageFileName, session, res);
 
     
 
